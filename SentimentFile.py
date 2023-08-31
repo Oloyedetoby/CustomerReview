67d6841221590735c7065f9c5a49cc75fd1eab55
Oloyedetoby <oloyedetoby@gmail.com>
2023-08-31 18:37:33 +0100

    first commit

---
+++ SentimentFile.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import sequence
+
+def main():
+    maxlen=100
+    st.title('Customer Sentiment Classifier')
+    review_description = st.text_area('Review Description:')
+    prediction  = st.button('Sentiment Prediction')
+    with open('tokenizing_text.pkl', 'rb') as fid:
+        tokenizer = pickle.load(fid)
+    sentiment_model = load_model('customize_conv_network.h5')
+
+
+    if prediction and review_description != '':
+        description = [review_description]
+        pred = sentiment_model.predict(sequence.pad_sequences(tokenizer.texts_to_sequences(description),maxlen=maxlen))  
+        if pred>0.8:
+            st.success(f'The sentiment is positive')
+        else:
+            st.error(f'The sentiment is negative')
+    elif prediction and review_description == '':
+        st.warning('Please enter your review')
+
+    
+
+if __name__=='__main__':
+    main()
